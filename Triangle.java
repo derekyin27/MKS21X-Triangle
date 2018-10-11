080656be465840f23a38162811ed8ebd513520f8
derekyin27 <43381977+derekyin27@users.noreply.github.com>
2018-10-10 21:03:48 -0400

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,1 +1,22 @@
-test
+public class Triangle {
+  private int v1;
+  private int v2;
+  private int v3;
+  
+  public Triangle(int vertex1, int vertex2, int vertex3) {
+    v1 = vertex1;
+    v2 = vertex2;
+    v3 = vertex3;
+  }
+  
+  public int getV1() {
+    return v1;
+  }
+  
+  public int getV2() {
+    return v2;
+  }
+  
+  public int getV3() {
+    return v3;
+  }
