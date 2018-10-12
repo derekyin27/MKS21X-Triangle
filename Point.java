fa7724ef29a13838d31fc9f0daae9e9357c84c36
derekyin27 <43381977+derekyin27@users.noreply.github.com>
2018-10-11 21:18:47 -0400

    Update Point.java

--- Point.java
+++ Point.java
@@ -18,4 +18,10 @@ public class Point{
   public double getY(){
     return y;
   }  
+public double distanceTo(Point p){
+    return Math.sqrt(Math.pow(this.getX() - p.getX(), 2) + Math.pow(p.getY() - this.getY(), 2));
+  }
+  public double distanceTo(Point x, Point y){
+    return Math.sqrt(Math.pow(x.getX() - y.getX(), 2) + Math.pow(x.getY() - y.getY(), 2));
+  }
 }
